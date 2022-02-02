2714bd8ae11a608c926894a5a8cc1834548d1af3
Subin Byun <sbyun95@gmail.com>
2022-02-02 19:52:38 +0900

    0202: FE setup comments

--- lena/src/server.js
+++ lena/src/server.js
@@ -1,4 +1,4 @@
-//setting Express
+//setting node set up using Express
 
 import express from "express";
 
@@ -8,6 +8,7 @@ app.set("view engine", "pug"); //config view engine to Pug
 app.set("views",__dirname + "/views"); //set the directory of views
 app.use("/public", express.static(__dirname + "/public")); //creat public url so the users can access (make it static)
 app.get("/", (req, res) => res.render("home")); //create route handler to render views (home.pug)
+app.get("/*", (req, res) => res.redirect("/")) // for all url redirect to "/" which is home in this case : catchAll
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 app.listen(3000, handleListen); //run port 3000 and run handleListen()
