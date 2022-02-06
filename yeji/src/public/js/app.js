37fef728f6343766609183ca3090186e3b8ed58d
yeji <yedoong919@gmail.com>
2022-02-06 23:43:09 +0900

    yeji_1.4

--- yeji/src/public/js/app.js
+++ yeji/src/public/js/app.js
@@ -1,21 +1,31 @@
-// 여기 frontend를 보면 버튼을 만들고, addEventListener를 만들 수 있다.
-// click 이벤트 생성 > callback 작성 || click 이벤트 생성 > 작동할 function 작성 (webSocket방식과 더 비슷 : webSocket도 event가 있고, event가 발동될 때 사용할 function 만들면 됨)
-// btn.addEventListener("click",fn)
+// frontend에서 socket을 만듦 + 3개의 event에 대해 listen 하기
 
-//frontend에 form이 있고, submit event를 listen한다고 하자.
-// JS는 function을 호출하고, 정보를 준다. (event정보와 같이 function을 호출할 것 + 어떤 일이 발생했는 지에 관한 정보)
-// webSocket은 listen할 특정한 event명이 있고, ws에서도 추가적인 정보를 받을 function이 존재함
-// form.addEventListener("click",fn)
-
-// const socket = new WebSocket("http://localhost:3000");
-//Uncaught DOMException: Failed to construct 'WebSocket': The URL's scheme must be either 'ws' or 'wss'. 'http' is not allowed 에러 발생
-// 다른 protocol이다. http가 아니다. ws(webSocket)이거나 wss(webSocket Secure)이어야 한다
-
-//const socket = new WebSocket("ws://localhost:3000");
-// localhost:3000 이라고 작성하는 대신 내가 어디에 있는지에 대한 정보를 주자. 현재는 그냥 다른 protocol일 뿐이다. 
-// 이것을 browser가 가져오게 만들기 -> JS에 옵션이 있다. : location.host => console창 > window.location.host
-
+// 새로고침 시, 4행의 코드가 실행
 const socket = new WebSocket(`ws://${window.location.host}`);
-// 이제 server로 접속할 수 있다
 
-// frontend에도 socket을 가지고 있음 -> frontend에서 backend로 메세지를 보내고 backend로부터 메세지를 받을 수 있다.
+// socket event 종류 : open, message, close, error
+
+// socket이 open 되었다면, browser에 연결되었다고 log에 출력하기
+socket.addEventListener("open", () => { // socket이 connection을 open했을 때 발생 (server와 연결되었을 때, 실행)
+    console.log("Connected to Server ✅");
+});
+
+// message를 받을 때마다 내용을 출력하는 message
+socket.addEventListener("message", (message) => {
+    // backend에서 frontend로 보낸 메세지를 받는 방법
+    // console.log("Just got this: ", message.data , " from the Server"); 
+    console.log("New message: ", message.data);
+});
+
+// server가 offline이 될 때 발생
+// close를 발생시키려면 서버를 disconnect해야한다. (서버를 꺼야한다.) => tab 나가기
+socket.addEventListener("close", () => {    // 서버로부터 연결이 끊겼을 때 발생
+    console.log("Disconnected from Server ❌");
+});
+// 브라우저의 연결이 끊기면, 서버에 (있는 socket의 )event를 발생시키고, 서버가 오프라인되면 브라우저한테 알려준다
+
+// frontend에서 backend로 메세지 보내기
+// 1. 메세지 보내기 (즉시 실행되지 않게 timeout 사용)
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
