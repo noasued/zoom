afe0b2454d880b4dac6344503b2c7326c2d10ba4
yeji <yedoong919@gmail.com>
2022-02-08 21:50:29 +0900

    0208_1.6 Chat Completed

--- yeji/src/public/js/app.js
+++ yeji/src/public/js/app.js
@@ -1,31 +1,36 @@
-// frontend에서 socket을 만듦 + 3개의 event에 대해 listen 하기
-
-// 새로고침 시, 4행의 코드가 실행
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-// socket event 종류 : open, message, close, error
-
-// socket이 open 되었다면, browser에 연결되었다고 log에 출력하기
-socket.addEventListener("open", () => { // socket이 connection을 open했을 때 발생 (server와 연결되었을 때, 실행)
+socket.addEventListener("open", () => { 
     console.log("Connected to Server ✅");
 });
 
-// message를 받을 때마다 내용을 출력하는 message
 socket.addEventListener("message", (message) => {
-    // backend에서 frontend로 보낸 메세지를 받는 방법
-    // console.log("Just got this: ", message.data , " from the Server"); 
     console.log("New message: ", message.data);
 });
 
-// server가 offline이 될 때 발생
-// close를 발생시키려면 서버를 disconnect해야한다. (서버를 꺼야한다.) => tab 나가기
-socket.addEventListener("close", () => {    // 서버로부터 연결이 끊겼을 때 발생
+socket.addEventListener("close", () => {    
     console.log("Disconnected from Server ❌");
 });
-// 브라우저의 연결이 끊기면, 서버에 (있는 socket의 )event를 발생시키고, 서버가 오프라인되면 브라우저한테 알려준다
 
-// frontend에서 backend로 메세지 보내기
-// 1. 메세지 보내기 (즉시 실행되지 않게 timeout 사용)
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+// message send하는 부분 우선 지우기
+// setTimeout(() => {
+//     socket.send("hello from the browser!");
+// }, 10000);
+
+// 1. function 생성 > message send하기
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    //console.log(input.value);
+    
+    // 2. 전에 사용했던 socket.send() 사용 => frontend의 form에서 backend로 무언가를 보내기
+    socket.send(input.value);
+
+    // 3. input.value 비워주기 (message send 후에 input 박스 비우기)
+    input.value = "";
+}
+
+
+messageForm.addEventListener("submit", handleSubmit);
--- yeji/src/server.js
+++ yeji/src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+// import { copyFileSync } from "fs";
 
 const app = express();
 
@@ -15,28 +16,52 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app); 
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket){ 
-    console.log(socket); 
+// function handleConnection(socket){ 
+//     console.log(socket); 
+// }
+
+function onSocketClose(){
+    console.log("Disconnected from the Browser ❌");
 }
 
-// connection 안에 위와 같은 역할을 하는 익명 함수 만들어주기 => 그러면 socket이 현재 어떤 상태인지 알기 쉽다 : connection이 생기면 socket을 받는다는 것을 알 수 있기 때문에
-// (event를 다룰 때도 이렇게 하는게 좋다)
-// socket에 있는 method 사용하기
+// function onSocketMessage(message){
+//     console.log(message);
+// }
+
+//두 개의 socket이 생긴다. => 이렇게 되면 메시지를 다른 모든 socket에 전달해줄 수 있다.
+const sockets = [];
+/*
+    brave가 connection되면 brave의 socket을 가져와서 array에 넣어준다.
+    그리고 firefox가 connection되면 그 socket을 넣어줌
+    brave에서 메시지를 받으면, 받은 메세지를 socket에 있는 모든 곳에 전달해줄 수 있다.
+*/
+
 wss.on("connection", (socket) => {
-    // 1. browser가 연결되면, 무언가를 console.log하고
-    console.log("Connected to Browser ✅");
+    //firefox가 연결될 때, firefox를 이 array에 넣어준다는 것 + brave가 연결될 때는 brave를 31행에 있는 array에 넣어줄 것
+    sockets.push(socket);
     
-    // 2. browser가 꺼졌을 때를 위한 listener 등록
-    socket.on("close", () => console.log("Disconnected from the Browser ❌")); // backend에서는 'close'라는 event를 listen하고 있다
-    
-    // 3. 브라우저가 서버에 메세지를 보냈을 때를 위한 listener 등록
-    // socket.on()을 작성하고, message 이벤트를 등록하기
-    socket.on("message", (message) => {
-        console.log(message);
+    console.log("Connected to Browser ✅");    
+    socket.on("close", onSocketClose);
+
+    // frontend input에서 보낸 message를 backend로 받았다. 그 message를 다시 frontend로 그대로 보내주기
+    socket.on("message", (message) =>{
+        /* aSocket : 위의 socket.on의 socket과는 다르다.
+                     각 브라우저를 aSocket으로 표시하고 메시지를 보낸다는 의미
+        */
+
+        //console.log(message.toString('utf8'));
+        sockets.forEach(aSocket => aSocket.send(message));
+        //socket.send(message);       // user로부터 message를 받아서 다시 돌려주고 있음
     });
-    
-    // 4. 브라우저에 메세지를 보내도록 작성
-    socket.send("hello!!!!");       // connection이 생겼을 때, socket으로 즉시 메세지를 보낸 것
+    /* chrome에서는 어떠한 메세지도 뜨지 않는다 (∴chrome에게 메시지를 보내지 않았기 때문에 올바르게 작동한 것)
+       firefox가 backend에 메시지를 전달했고, 서버가 firefox에 답장을 해줌
+       한 개의 서버가 서로 다른 두 브라우저로부터 메시지를 받고 있지만, 서로 다른 브라우저는 서로 메세지를 주고받지 못한다.
+       wss.on 안에 있는 코드들은 firefox와 brave에서 모두 작동한다. => firefox가 서버에 메시지를 보내면, 서버는 firefox에게 답장을 해준다 => wss.on안의 코드들은 brave와 connectino될 때와 firefox와 connection될 때에도 작동(총 2번 작동한다.)
+       => brave와 서버 사이에 websocket이 있고, firefox와 서버 사이에도 websocket이 있다.
+       현재는 서버와 브라우저 둘이서만 주고받고 있음 -> 그래서 서버가 firefox로부터 메세지를 받아서 brave에 전달해줄 것 (이러기 위해서는 누가 연결되었는지 알아야 한다.)
+       => fake database를 만들어주고, 누군가 내 서버에 연결하면 그 connection을 여기에 넣는다
+    */
+//    socket.send("hello!!!!");
 });
 
 server.listen(3000, handleListen);
