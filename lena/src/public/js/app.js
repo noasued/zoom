a066de19ecaaa8eaabd198ae66e978d11d711196
Subin Byun <sbyun95@gmail.com>
2022-02-16 17:53:58 +0900

    0216: 3.1 Call Controls

--- lena/src/public/js/app.js
+++ lena/src/public/js/app.js
@@ -1,13 +1,35 @@
 const socket = io();
+
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
-const cameraBtn = document.getElementById("camera")
+const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 //Create Stream:: stream : video + audio
 let myStream;
 let muted = false;
 let camOff = false;
 
+//유저의 카메라 정보 가져오기
+async function getCameras(){
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => (device.kind === "videoinput"));
+
+        //카메라 목록 보여주기
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            cameraSelect.appendChild(option);
+        });
+    } 
+    catch (e) {
+        console.log(e);
+    }
+}
+
+//유저의 stream 정보 가져오기
 async function getMedia(){
     try{
         myStream = await navigator.mediaDevices.getUserMedia(
@@ -17,6 +39,7 @@ async function getMedia(){
             }
         );
             myFace.srcObject = myStream;
+            await getCameras();
     }
     catch(e){
         console.log(e);
@@ -25,7 +48,9 @@ async function getMedia(){
 
 getMedia();
 
+//음소거
 function handleMuteClick(){
+    myStream.getAudioTracks().forEach(track => (track.enabled = !track.enabled));
     if(!muted){
         muteBtn.innerHTML = "Unmute";
         muted = true;
@@ -36,7 +61,9 @@ function handleMuteClick(){
     }
 };
 
+//카메라 onoff
 function handleCamClick(){
+    myStream.getVideoTracks().forEach(track => (track.enabled = !track.enabled));
     if(camOff){
         cameraBtn.innerHTML = "Turn off Camera"
         camOff = false;
