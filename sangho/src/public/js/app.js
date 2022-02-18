9f3df1e761f813453a4bec3abd73e1b2db9600c5
siren0416 <78391171+siren0416@users.noreply.github.com>
2022-02-18 19:05:22 +0900

    ch3_update

--- sangho/src/public/js/app.js
+++ sangho/src/public/js/app.js
@@ -4,26 +4,65 @@ const myFace = document.getElementById("myFace");
 // stream : 비디오 오디오 연결된 것
 const muteBtn = document.getElementById("mute");
 const cameraBtn =document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
-async function getMedia() {
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentcamera = myStream.getVideoTracks()[0];
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentcamera.label === camera.label){
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
+        })
+    }catch (e){
+        console.log(e);
+    }
+}
+
+
+
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio : true,
+        video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+        audio : true,
+        video: { deviceId: { exact: deviceId } },
+    }
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({
-                audio: true,
-                video: true,
-            });
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrains
+        );
             myFace.srcObject = myStream;
+            if(!deviceId){
+                await getCameras();
+            }
     } catch (e) {
         console.log(e);
     }
 }
 
-getMedia();
-
 function handlemuteClick(){
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
     if(!muted){
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -33,6 +72,7 @@ function handlemuteClick(){
     }
 }
 function handlecameraClick(){
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
     if(cameraOff){
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
@@ -42,8 +82,57 @@ function handlecameraClick(){
     }
 }
 
+async function handleCameraChange(){
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handlemuteClick);
 cameraBtn.addEventListener("click", handlecameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+// WelcomeForm (join  a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia(){
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+
+}
+
+
+function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome",  async () =>{
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", offer =>{
+    console.log(offer);
+})
+
+// RTC Code
+
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection();
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
 // socket io 내용
 // const socket = io();
 
