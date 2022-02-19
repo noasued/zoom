f9ee0d9b93f7c882c626e01dd82986556a5e1454
yeji <yedoong919@gmail.com>
2022-02-19 15:59:42 +0900

    yeji_0219_Chapter2

--- yeji/src/public/js/app.js
+++ yeji/src/public/js/app.js
@@ -1,13 +1,10 @@
 const socket = io();
 const welcome = document.getElementById("welcome")
 const form = welcome.querySelector("form");
-
 const room = document.getElementById("room");
 room.hidden = true;
-
 let roomName;
 
-// 메세지를 추가해주는 function 생성
 function addMessage(message){
     const ul = room.querySelector("ul")
     const li = document.createElement("li");
@@ -19,12 +16,32 @@ function backendDone(msg){
     console.log(`The backend says `, msg );
 }
 
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message",input.value, roomName, () =>{ 
+        addMessage(`You: ${input.value}`);
+    });
+    input.value = "";
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+}
+
 function showRoom(){
     welcome.hidden = true;
     room.hidden = false;
-
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function handleRoomSubmit(event){
@@ -34,12 +51,33 @@ function handleRoomSubmit(event){
     roomName = input.value;
     input.value="";
 }
+
 form.addEventListener("submit", handleRoomSubmit);
 
-// 4. event에 반응하도록 frontend도 만들어주기
-socket.on("welcome", () => {
-    const ul = room.querySelector("ul")
-    const li = document.createElement("li");
-    li.innerText = "Someone joined!"
-    ul.appendChild(li);
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText=`Room ${roomName} (${newCount})`;
+    addMessage(`${user} arrived!`);
+});
+
+socket.on("bye", (left) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText=`Room ${roomName} (${newCount})`;
+    addMessage(`${left} left ㅠㅠ`);
+});
+
+socket.on("new_message", (msg)=> {addMessage(msg)});
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    
+    if(rooms.length === 0){
+        roomList.innerHTML = ""; 
+        return;
+    }
+    rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
 });
